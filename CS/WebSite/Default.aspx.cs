b2ad6fea10c11de0639263647c985a17010afafe
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 18:31:47 +0400

    Change example status to out of maintenance

--- CS/WebSite/Default.aspx.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public partial class _Default : System.Web.UI.Page {
-    protected void Page_Init(object sender, EventArgs e) {
-        xds.Session = XpoHelper.GetNewSession();
-    }
-}
--- VB/WebSite/Default.aspx.vb
+++
@@ -1,9 +0,0 @@
-Imports Microsoft.VisualBasic
-Imports System
-
-Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
-		xds.Session = XpoHelper.GetNewSession()
-	End Sub
-End Class
